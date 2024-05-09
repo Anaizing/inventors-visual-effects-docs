ab3bb36a9272de84cec9e1250338681a501891f5
Ian Araya <anai.yusary@gmail.com>
2024-05-09 20:51:39 +1000

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,61 @@
+import { defineConfig } from "astro/config";
+import { remarkModifiedTime } from "./src/utils/remark-modified-time.mjs";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
+import pagefind from "astro-pagefind";
+import icon from "astro-icon";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astroverse.inote.xyz/",
+  trailingSlash: "always",
+  output: "static",
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
+  },
+  experimental: {
+    contentCollectionCache: true,
+  },
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.unsplash.com",
+      },
+    ],
+  },
+  markdown: {
+    remarkPlugins: [remarkModifiedTime],
+  },
+  integrations: [
+    mdx(),
+    sitemap(),
+    pagefind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+        debug: false,
+      },
+    }),
+    icon({
+      include: {
+        tabler: ["*"],
+        "flat-color-icons": [
+          "template",
+          "gallery",
+          "approval",
+          "document",
+          "advertising",
+          "currency-exchange",
+          "voice-presentation",
+          "business-contact",
+          "database",
+        ],
+      },
+    }),
+    tailwind(),
+  ],
+});
